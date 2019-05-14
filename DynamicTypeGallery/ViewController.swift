8b6e1047c9833341a0a24bace746b0958c7ae246
Kuba Suder <jakub.suder@gmail.com>
2019-05-14 16:13:28 +0200

    added labels showing UIFont styles

--- DynamicTypeGallery/ViewController.swift
+++ DynamicTypeGallery/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let fontStyles: [UIFont.TextStyle] = [
+            .body,
+            .callout,
+            .caption1,
+            .caption2,
+            .footnote,
+            .headline,
+            .largeTitle,
+            .subheadline,
+            .title1,
+            .title2,
+            .title3
+        ]
+
+        for style in fontStyles {
+            let font = UIFont.preferredFont(forTextStyle: style)
+            let styleName = style.rawValue.replacingOccurrences(of: "UICTFontTextStyle", with: "")
+
+            let label = UILabel()
+            label.text = "\(styleName) (\(font.fontName) \(font.pointSize)pt)"
+            label.font = font
+            label.numberOfLines = 0
+
+            stackView.addArrangedSubview(label)
+        }
     }
-
-
 }
 
